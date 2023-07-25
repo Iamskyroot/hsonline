908e0b69aaeb40ad11f6dfe6ee32c882fa8a3616
iamskyroot <mimovil547@gmail.com>
2023-07-25 20:23:41 +0100

    creating repository

---
+++ src/main/java/com/tfg/hsonline/HsonlineApplication.java
@@ -0,0 +1,13 @@
+package com.tfg.hsonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsonlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tfg/hsonline/HsonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfg.hsonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsonlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
